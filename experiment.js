981f913e42d71cd8dac9ba616bba453cc6259517
Vaishnavi Pukale <vaishnavipukale@Vaishnavis-MacBook-Air.local>
2024-10-26 18:28:42 +0530

    my First Commit

---
+++ experiment.js
@@ -0,0 +1,37 @@
+/*console.log("hello world");
+
+const monthlyRent = 600;
+const yearlyRent = monthlyRent * 12;
+console. log(yearlyRent);
+const myName = "Vaishnavi Pukale"
+console. log(myName);
+const fisrtName = "vaishnavi"
+const lastName = "Pukale"
+
+const sentenceWithTemplete = `Hello ${fisrtName} ${lastName}! How are you!?`
+
+console. log(sentenceWithTemplete);
+const skyIsBlack = false;
+
+if (skyIsBlack) {console. log("The sky is Black");} 
+else
+{console. log("The sky is not black its blue");}
+
+if (2+2 === 7) {
+    console. log("Oh thank god, the fundamental principles of maths still hold true");
+}
+else {
+    console. log("ohh,panic?");
+}
+*/
+const timesToRepeat = 50;
+const character = '🥰😇'
+
+let answer = " ";
+
+for (let i = 0; i< timesToRepeat; i++) {
+    answer = answer + character ;
+
+}
+
+console. log(answer)
